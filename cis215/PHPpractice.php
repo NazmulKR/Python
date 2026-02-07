cf2cf1d94257032ffca6fbb8d0f626694b76431c
Nazmul <kabirnazmul143@gmail.com>
2026-02-07 15:46:54 -0500

    feat: adding type juggling and condition

--- cis215/PHPpractice.php
+++ cis215/PHPpractice.php
@@ -13,4 +13,11 @@ echo "<br>";
 
 // Adding to the variable itself
 echo $a += 2; // Outputs: 4 (because 2 + 2 = 4)
+echo "<br>";
+echo "10"+ $b;// Type Juggling
+ echo "<br>";
+ echo true + $c;
+ if ($a != $c) {
+    echo "This stat3ement is True";
+ } //Condioinal
 ?>
