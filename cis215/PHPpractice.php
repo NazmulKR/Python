1933545af43681973acf808367c7f5ab92573b2a
Nazmul <kabirnazmul143@gmail.com>
2026-02-07 15:40:36 -0500

    feat: adding more php and removing str

--- cis215/PHPpractice.php
+++ cis215/PHPpractice.php
@@ -1,3 +1,16 @@
 <?php
-$name = "Naz";
+$a = 2;
+$b = 2;
+
+// Joining numbers as a string (Concatenation)
+$c = $a . " " . $b; 
+echo $c; // Outputs: 2 2
+echo "<br>";
+
+// Adding numbers together (Math)
+echo $a + $b; // Outputs: 4
+echo "<br>";
+
+// Adding to the variable itself
+echo $a += 2; // Outputs: 4 (because 2 + 2 = 4)
 ?>
