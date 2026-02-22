eafa68f87d77bf4cf056d0b8c2e3f0297d8a5e58
Nazmul <kabirnazmul143@gmail.com>
2026-02-21 21:25:53 -0500

    adding boilerplate

--- cis215/hw4/car_quiz.php
+++ cis215/hw4/car_quiz.php
@@ -7,3 +7,14 @@ This file shows a simple car themed form.
 The user answers questions about cars amd answers sent to car_results.php file
 */
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Car Personality Quiz</title>
+</head>
+<body>
+    
+</body>
+</html>
