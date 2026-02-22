7ad7f07e8e092730c7c7562d2297ef7e18dd9f97
Nazmul <kabirnazmul143@gmail.com>
2026-02-21 21:23:58 -0500

    adding php sytax and comment about the project

---
+++ cis215/hw4/car_quiz.php
@@ -0,0 +1,13 @@
+<?php
+/* Homework 4 - Car Personality Quiz 
+Nazmul Kabir Rahive 
+email : nrahive@genesee.edu
+
+This file shows a simple car themed form.
+The user answers questions about cars amd answers sent to car_results.php file
+*/
+
+
+
+
+?>
