93dde515a5ca3b841663cfb117558af65e6b2161
Nazmul <kabirnazmul143@gmail.com>
2026-02-23 12:08:54 -0500

    testing html inside echo

---
+++ cis215/hw4_pdated/homework4_results.php
@@ -0,0 +1,29 @@
+<?php
+/*
+Nazmul Kabir Rahive
+nrahive@genesee.edu
+This file recieves the data entered by the user by the form
+*/
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Quiz Results</title>
+</head>
+<body>
+    <main>
+        <?php
+         function check_required($list) {
+            foreach ($list as $items) {
+                if (!isset($_POST[$items]) and $_POST[$items] ==="") {
+                    echo"<h2>Error</h2>";
+                    echo"<p>You must answer all questions.</p>";
+                    echo "<p> <a href=\"homework4_quiz.php\">Go back</a><p>";
+
+        ?>
+    </main>
+    
+</body>
+</html>
