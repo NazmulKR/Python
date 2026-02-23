bc4a9d9f983936f5d008ad8d07c99759ff4d16c0
Nazmul <kabirnazmul143@gmail.com>
2026-02-23 11:31:39 -0500

    adding html

---
+++ cis215/hw4_pdated/homework4_quiz.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Video Game Personality Quiz</title>
+</head>
+<body>
+    <main>
+        <form action="homeowork4_results.php" method="POST">
+          <fieldset>
+            <legend>1. What is your favorite game genre?</legend>
+            <div> <input type="radio" name="q1" id="q1-action" value="action"> <label for="q1-action">Action</label> </div>
+            
+          </fieldset>
+
+
+
+
+        </form>
+    </main>
+</body>
+</html>
