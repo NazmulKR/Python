152d4783935de6f16f5d2b205cf04b880fe64705
Nazmul <kabirnazmul143@gmail.com>
2026-02-23 11:35:28 -0500

    adding comments through php

--- cis215/hw4_pdated/homework4_quiz.php
+++ cis215/hw4_pdated/homework4_quiz.php
@@ -1,3 +1,10 @@
+<?php
+/* Nazmul Kabir Rahive
+nrahive@genesee.edu
+This file shows a Game themed form.
+The user answers questions about games and answers sent to homework4_results.php file
+*/
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,7 +17,7 @@
           <fieldset>
             <legend>1. What is your favorite game genre?</legend>
             <div> <input type="radio" name="q1" id="q1-action" value="action"> <label for="q1-action">Action</label> </div>
-            
+
           </fieldset>
 
 
