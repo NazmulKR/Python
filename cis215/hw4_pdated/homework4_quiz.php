d74f6de13c9c25ded51dcc7553cee6de3c8f3532
Nazmul <kabirnazmul143@gmail.com>
2026-02-25 22:15:34 -0500

    adding final html

--- cis215/hw4_pdated/homework4_quiz.php
+++ cis215/hw4_pdated/homework4_quiz.php
@@ -22,15 +22,15 @@ The user answers questions about games and answers sent to homework4_results.php
           </fieldset>
           <fieldset>
             <legend>2. How do you approach a boss fight?</legend>
-            <div><input type="radio" name="q2" id="q2-rush" value="rush"> <label for="q2-rush">Rush in and attack</label></div>
+            <div><input type="radio" name="q2" id="q2-rush" value="rush"> 
+            <label for="q2-rush">Rush in and attack</label>
+             </div>
             <div><input type="radio" name="q2" id="q2-plan" value="plan"> <label for="q2-plan">Study patterns and plan</label></div>
           </fieldset>
           <fieldset>
-            <legend>
-                3. Choose your favorite in-game item
-            </legend>
-
+             
             <div>
+                <label for="q3-select">Choose your favorite in-game item:</label>
                 <select name="q3" id="q3-select">
                     <option value="default">-- Select an item --</option>
                     <optgroup label="Common Items">
