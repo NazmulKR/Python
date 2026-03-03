091f4f91b3ab1e52565a138ab9645804305a1a4f
Nazmul <kabirnazmul143@gmail.com>
2026-03-02 23:57:46 -0500

    add: variablesand POST for collecting data

---
+++ cis215/project1/process_survey.php
@@ -0,0 +1,18 @@
+<?php
+
+include "dbconfig.php";
+
+$survey_password = $_POST["survey_password"];
+$email = $_POST["email"];
+$age = $_POST["age"];
+$gender = $_POST["gender"];
+$major = $_POST["major"];
+
+
+
+
+
+
+
+
+?>
