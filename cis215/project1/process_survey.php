1f203894519f4fd18c81b164ed20b010190426cb
Nazmul <kabirnazmul143@gmail.com>
2026-03-03 23:02:54 -0500

    add validation password email age

--- cis215/project1/process_survey.php
+++ cis215/project1/process_survey.php
@@ -20,7 +20,29 @@ $HASH = '$2y$10$86U6eOu77G6diGRvy42YhuwJyVohlGz6MvB5mamDLgJQSQXmdr3ce';
 
 $errors = [];
 
+if (!password_verify($survey_password, $HASH)) {
+    $errors[] = "Incorrect survey password.";
+}
 
+$email = trim($email);
+if ($email == "" || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    $errors[] = "Please enter a valid email.";
+}
+
+$allowed_ages = [
+    "18-22","23-27","28-32","33-37",
+    "38-42","43-47","48-52","53-57",
+    "58-62","63-67","68+"
+];
+$age_ok = false;
+foreach ($allowed_ages as $a) {
+    if ($age == $a) {
+        $age_ok = true;
+    }
+}
+if ($age_ok == false) {
+    $errors[] = "Please select a valid age.";
+}
 
 
 ?>
