0c8a31b0ba5634c6ad289605ed9b87bd68ea7e37
Nazmul <kabirnazmul143@gmail.com>
2026-03-03 22:47:33 -0500

    add : finishing form fields

--- cis215/project1/process_survey.php
+++ cis215/project1/process_survey.php
@@ -7,6 +7,8 @@ $email = $_POST["email"];
 $age = $_POST["age"];
 $gender = $_POST["gender"];
 $major = $_POST["major"];
+$hours           = $_POST["hours_per_week"];
+$comments        = $_POST["comments"];
 
 
 
