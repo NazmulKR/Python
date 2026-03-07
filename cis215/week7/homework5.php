53af93c547571177944e01a76d908aae6c807444
Nazmul <kabirnazmul143@gmail.com>
2026-03-07 03:38:01 -0500

    feat : create table

--- cis215/week7/homework5.php
+++ cis215/week7/homework5.php
@@ -11,6 +11,17 @@ $db = connectDB();
 
 echo "<h1>Movie Database</h1>";
 
+$sql_create = "CREATE TABLE movies (
+id INT AUTO_INCREMENT PRIMARY KEY,
+title VARCHAR(100),
+rating INT,
+runtime INT,
+director VARCHAR(100),
+release_year INT
+)";
+
+$db->query($sql_create);
+
 
 
 
