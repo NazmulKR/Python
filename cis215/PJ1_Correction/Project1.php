b09f26084644f729cfa6d9539f0ee7eb80e9bd21
Nazmul <kabirnazmul143@gmail.com>
2026-03-23 14:18:09 -0400

    feat : add boiler plate & heading

---
+++ cis215/PJ1_Correction/Project1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Student Study Habits Survey</title>
+</head>
+<body>
+
+<header>
+    <h1>Student Study Habits Survey</h1>
+    <p>Please complete this survey about your study habits and time management.</p>
+</header>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
