12bc7746445d32cf6521329e27bcc174db6c91c9
Nazmul <kabirnazmul143@gmail.com>
2026-03-23 15:25:12 -0400

    feat: add survey input handling in process_survey.php

--- cis215/PJ1_Correction/process_survey.php
+++ cis215/PJ1_Correction/process_survey.php
@@ -1,5 +1,12 @@
 <?php
 include "dbconfig.php";
 
+$survey_password = $_POST["survey_password"];
+$email = trim($_POST["email"]);
+$age = $_POST["age"];
+$gender = $_POST["gender"];
+$major = trim($_POST["major"]);
+$hours = $_POST["hours_per_week"];
+$comments = trim($_POST["comments"]);
 
 ?>
