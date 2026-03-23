96fffad50c2d6fe7282fdb234725ad0367598627
Nazmul <kabirnazmul143@gmail.com>
2026-03-23 14:44:19 -0400

    feat: add age range options to survey form

--- cis215/PJ1_Correction/Project1.php
+++ cis215/PJ1_Correction/Project1.php
@@ -30,9 +30,23 @@
             <input type="password" id="survey_password" name="survey_password" required>
         </div>
     </fieldset>
-    
-
-
+    <fieldset>
+        <legend>About You</legend>
+        <div>
+            <p>Age Range</p>
+             <label><input type="radio" name="age" value="18-22" required>18-22</label>
+                <label><input type="radio" name="age" value="23-27">23-27</label>
+                <label><input type="radio" name="age" value="28-32">28-32</label>
+                <label><input type="radio" name="age" value="33-37">33-37</label>
+                <label><input type="radio" name="age" value="38-42">38-42</label>
+                <label><input type="radio" name="age" value="43-47">43-47</label>
+                <label><input type="radio" name="age" value="48-52">48-52</label>
+                <label><input type="radio" name="age" value="53-57">53-57</label>
+                <label><input type="radio" name="age" value="58-62">58-62</label>
+                <label><input type="radio" name="age" value="63-67">63-67</label>
+                <label><input type="radio" name="age" value="68+">68+</label>
+        </div>
+    </fieldset>
 
 
 
