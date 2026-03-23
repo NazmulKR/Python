a2bfc373f7737954a12baab82fd52ded9584ecfa
Nazmul <kabirnazmul143@gmail.com>
2026-03-23 15:22:04 -0400

    fix: comment out debug echo in database connection

--- cis215/PJ1_Correction/dbconfig.php
+++ cis215/PJ1_Correction/dbconfig.php
@@ -9,7 +9,7 @@ function connectDB(){
     $host = "localhost";
     try {
         $db = new PDO("mysql:host=$host;dbname=$database", $user, $pass);
-        echo "db success";    # You'll need to take this echo out when you know it's working
+        //echo "db success";    # You'll need to take this echo out when you know it's working
     }
     catch (PDOException $e) {echo $e;}
     return $db; } ?>
