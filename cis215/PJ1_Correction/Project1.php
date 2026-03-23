343996b6ef72efd13f05eff9aa6f3aaab3db4589
Nazmul <kabirnazmul143@gmail.com>
2026-03-23 14:20:45 -0400

    feat : add navigation bar & link

--- cis215/PJ1_Correction/Project1.php
+++ cis215/PJ1_Correction/Project1.php
@@ -10,6 +10,12 @@
     <h1>Student Study Habits Survey</h1>
     <p>Please complete this survey about your study habits and time management.</p>
 </header>
+<nav>
+    <ul>
+        <li><a href="survey.php">Survey Form</a></li>
+        <li><a href="data.php">View Survey Data</a></li>
+    </ul>
+</nav>
 
 
 
