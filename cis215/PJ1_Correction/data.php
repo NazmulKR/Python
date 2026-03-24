90ca7aab168400be7684a01d8fb5d73266ee85f4
Nazmul <kabirnazmul143@gmail.com>
2026-03-23 23:12:42 -0400

    feat: enhance survey data display

--- cis215/PJ1_Correction/data.php
+++ cis215/PJ1_Correction/data.php
@@ -15,6 +15,26 @@ $stmt = $db->query($sql);
     <title>Survey Data</title>
 </head>
 <body>
-    
+    <main>
+         <h1>Survey Data</h1>
+         <a href="survey.php">Return to Survey</a>
+         <?php
+           $row = $stmt->fetch() ;
+              while ($row) {
+                echo "<ul>";
+            echo "<li>Email: " .($row["email"]) . "</li>";
+            echo "<li>Age: " .($row["age_range"]) . "</li>";
+            echo "<li>Gender: " .($row["gender"]) . "</li>";
+            echo "<li>Major: " .($row["major"]) . "</li>";
+            echo "<li>Hours: " .($row["hours_per_week"]) . "</li>";
+            echo "<li>Methods: " .($row["study_methods"]) . "</li>";
+            echo "<li>Comments: " .($row["comments"]) . "</li>";
+            echo "</ul>";
+            
+        }
+                
+         ?>
+
+    </main>
 </body>
 </html>
